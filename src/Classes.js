5a2c3e0cec675550baac482ca8bda9e98e547477
Patrick Ly <112744444447551+patrickly1@users.noreply.github.com>
2024-06-06 00:24:27 -0400

    added placeShip method to the Gameboard class that updates the board and Cell class with the Ship class. Addded unit testing to see if ships are placed correctly

--- src/Classes.js
+++ src/Classes.js
@@ -1,4 +1,7 @@
-export { Ship }
+export { Ship,
+    Cell,
+    Gameboard
+ }
 
 class Ship {
     constructor(length) {
@@ -21,10 +24,56 @@ class Ship {
 
 class Cell {
     constructor() {
+        this.hit = false;
+        this.ship = null;
+    }
 
+    placeShip(ship) {
+        this.ship = ship;
+    }
+
+    attack() {
+        this.hit = true;
+        if (this.ship) {
+            this.ship.attacked();
+        }
     }
 }
 
 class Gameboard {
-    
+    constructor() {
+        this.rows = 7;
+        this.columns = 8;
+        this.board = this.generateBoard();
+    }
+
+    generateBoard() {
+        let board = []
+        for (let i = 0; i < this.rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < this.columns; j++) {
+                board[i].push(new Cell());
+            }
+        }
+        return board;
+    }
+
+    placeShip(ship, orientation, x, y) {
+        if (orientation === "vertical") {
+            if (y + ship.length > this.rows) {
+                return false
+            }
+            for (let i = 0; i < ship.length; i++) {
+                this.board[y + i][x].placeShip(ship);
+            }
+        }
+        if (orientation === "horizontal") {
+            if (x + ship.length > this.columns) {
+                return false;
+            }
+            for (let j = 0; j < ship.length; j++) {
+                this.board[y][x + j].placeShip(ship);
+            }
+        }
+    }
 }
--- src/test.test.js
+++ src/test.test.js
@@ -1,4 +1,7 @@
-import { Ship } from "./Classes";
+import { Ship,
+    Cell, 
+    Gameboard
+ } from "./Classes";
 
 it("Ship not initially sunk", () => {
     let newShip = new Ship(3);
@@ -11,4 +14,55 @@ it("Ship sunk after receiving enough hits", () => {
     newShip.attacked()
     newShip.attacked();
     expect(newShip.isSunk()).toBe(true);
+})
+
+it("Gameboard populated", () => {
+    let newGameBoard = new Gameboard();
+    expect(newGameBoard.board.length).toBe(7);
+    newGameBoard.board.forEach(row => {
+        expect(row.length).toBe(8);
+    })
+})
+
+it("Gameboard square has cell class", () => {
+    let newGameBoard = new Gameboard();
+    expect(newGameBoard.board[0][0]).toBeInstanceOf(Cell);
+})
+
+it("Placing the ship onto cells vertically", () => {
+    let newGameBoard = new Gameboard();
+    let newShip = new Ship(3);
+    newGameBoard.placeShip(newShip, "vertical", 0, 0)
+    //Should have a ship
+    expect(newGameBoard.board[0][0].hit).toBeFalsy();
+    expect(newGameBoard.board[0][0].ship).not.toBeNull();
+    expect(newGameBoard.board[1][0].hit).toBeFalsy();
+    expect(newGameBoard.board[1][0].ship).not.toBeNull();
+    expect(newGameBoard.board[2][0].hit).toBeFalsy();
+    expect(newGameBoard.board[2][0].ship).not.toBeNull();
+    //Should not have a ship
+    expect(newGameBoard.board[3][0].hit).toBeFalsy();
+    expect(newGameBoard.board[3][0].ship).toBeNull();
+})
+
+it("Placing the ship onto cells horizontally", () => {
+    let newGameBoard = new Gameboard();
+    let newShip = new Ship(6);
+    newGameBoard.placeShip(newShip, "horizontal", 1, 1)
+    //Should have a ship
+    expect(newGameBoard.board[0][0].hit).toBeFalsy();
+    expect(newGameBoard.board[0][0].ship).toBeNull();
+    expect(newGameBoard.board[1][1].hit).toBeFalsy();
+    expect(newGameBoard.board[1][1].ship).not.toBeNull();
+    expect(newGameBoard.board[1][2].hit).toBeFalsy();
+    expect(newGameBoard.board[1][2].ship).not.toBeNull();
+    expect(newGameBoard.board[1][3].hit).toBeFalsy();
+    expect(newGameBoard.board[1][3].ship).not.toBeNull();
+    expect(newGameBoard.board[1][4].hit).toBeFalsy();
+    expect(newGameBoard.board[1][4].ship).not.toBeNull();
+    expect(newGameBoard.board[1][5].hit).toBeFalsy();
+    expect(newGameBoard.board[1][5].ship).not.toBeNull();
+    //Should not have a ship
+    expect(newGameBoard.board[1][6].hit).toBeFalsy();
+    expect(newGameBoard.board[1][6].ship).not.toBeNull();
 })
